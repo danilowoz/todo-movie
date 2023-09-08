69a98c0298bf5d4deb00e0a5ecbbc0e717ea9ec1
Danilo Woznica <danilowoz@gmail.com>
2023-09-08 19:44:34 +0000

    Update Search.svelte and search.css

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,17 +1,50 @@
 <script lang="ts">
   import './search.css';
 
+  let controller = new AbortController();
+  let signal = controller.signal;
+
   let query = '';
   let predictTerm = null;
   let cursorOffset = 0;
+  let results = [];
 
   async function search(query) {
-    const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=7ab33eb9`);
-    const data = await response.json();
+    try {
+      // abort previous request
+      controller.abort();
 
-    if (data.Search && data.Search[0].Title.match(new RegExp('^' + query, 'gi'))) {
-      predictTerm = data.Search[0];
+      // reassign signal
+      controller = new AbortController();
+      signal = controller.signal;
+
+      const response = await fetch(`https://www.omdbapi.com/?s=${query.trim()}&apikey=7ab33eb9`, {
+        signal
+      });
+      const data = await response.json();
+
+      if (data.Search && data.Search[0].Title.match(new RegExp('^' + query, 'gi'))) {
+        predictTerm = data.Search[0];
+      }
+
+      results = data.Search ? data.Search : [];
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  function tabHandler(event) {
+    if (event.code === 'Tab') {
+      query = predictTerm?.Title;
+      predictTerm = null;
+
+      search(query);
+      event.preventDefault();
+    }
+  }
+
+  function disposeTabHandler() {
+    window.removeEventListener('keydown', tabHandler);
   }
 
   function onSearch(event: Event) {
@@ -24,20 +57,28 @@
 
     search(query);
 
-    window.addEventListener('keydown', (event) => {
-      if (event.code === 'Tab') {
-        query = predictTerm.Title;
-        predictTerm = null;
-        event.preventDefault();
-      }
-    });
+    window.addEventListener('keydown', tabHandler);
   }
 </script>
 
 <div class="app-search" style="--cursor-offset: {cursorOffset}px">
-  <input bind:value={query} on:input={onSearch} type="search" placeholder="Search..." />
+  <input
+    bind:value={query}
+    on:input={onSearch}
+    on:blur={disposeTabHandler}
+    type="search"
+    placeholder="Search..."
+  />
   <span class="app-search_query">{query}</span>
   {#if predictTerm}
     <span class="app-search_predict">{predictTerm.Title.replace(new RegExp(query, 'gi'), '')}</span>
   {/if}
+
+  <div class="app-search_results">
+    {#each results as result}
+      {#if result.Title}
+        <div class="app-search_result">{result.Title}</div>
+      {/if}
+    {/each}
+  </div>
 </div>
