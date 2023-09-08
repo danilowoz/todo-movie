3a1a8dbc517cede66c30ca9b2d09639f765ecabb
Danilo Woznica <danilowoz@gmail.com>
2023-09-08 18:31:31 +0000

    Update Tabs.svelte and tabs.css

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -3,12 +3,26 @@
 
   let items = ['Default', 'Last added', 'Unseen'];
   let current = items[0];
+
+  let leftPosition = 4;
+  let rightPosition = 159;
+
+  function setActiveItem({ target }) {
+    current = target.innerText;
+
+    leftPosition = target.offsetLeft;
+    rightPosition = target.parentElement.offsetWidth - target.offsetLeft - target.offsetWidth;
+  }
 </script>
 
 <div>
-  <nav class="app-nav">
+  <nav class="app-nav" style="--left: {leftPosition}px; --right: {rightPosition}px;">
     {#each items as item}
-      <button class="app-nav_item" class:app-nav_item--active={current === item}>{item}</button>
+      <button
+        on:click={setActiveItem}
+        class="app-nav_item"
+        class:app-nav_item--active={current === item}>{item}</button
+      >
     {/each}
   </nav>
 </div>
